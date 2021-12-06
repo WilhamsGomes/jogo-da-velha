401e437b2bc63d7002ae2edb2b358ca2141e62ba
WilhamsGomes <wilhamsgoomes@gmail.com>
2021-12-05 22:57:54 -0300

    Enviando projeto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Game from './Game';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Game/>
+    </div>
+  );
+}
+
+export default App;
